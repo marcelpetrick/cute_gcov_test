fef80001e488da25e9850dae223e471febcc356b
Marcel Petrick <mail@marcelpetrick.it>
2021-11-29 18:23:05 +0100

    runCoverage.sh: several improvements

--- scripts/runCoverage.sh
+++ scripts/runCoverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
 ############################################################################################################
-# You should edit the following 3 paths when necessary
+# You should edit the following three paths if necessary.
 ############################################################################################################
 
 # Get the path to the current folder
@@ -18,20 +18,20 @@ COV_DIR="$SCRIPT_DIR/../coverage"
 # Path where the HTML files should be saved
 HTML_RESULTS="${COV_DIR}""/html"
 
-# Create the html folder if it does not exists
+# Create the html folder if it does not exist
 mkdir -p ${HTML_RESULTS}
 
 # Generate our initial info
 lcov -d "${SRC_DIR}" -c -o "${COV_DIR}/coverage.info"
  
 # Remove some paths/files which we don't want to calculate the code coverage (e.g. third party libraries) and generate a new coverage file filtered (feel free to edit it when necessary)
-lcov -r "${COV_DIR}/coverage.info" "*Qt*.framework*" "*.h" "*/tests/*" "*Xcode.app*" "*.moc" "*moc_*.cpp" "*/test/*" "*/build*/*" -o "${COV_DIR}/coverage-filtered.info"
+lcov -r "${COV_DIR}/coverage.info" "*Qt*.framework*" "*.h" "*/tests/*" "*Xcode.app*" "*.moc" "*moc_*.cpp" "*/test/*" "*/build*/*" "*/c++/*" -o "${COV_DIR}/coverage-filtered.info"
  
 # Generate the HTML files
-genhtml -o "${HTML_RESULTS}" "${COV_DIR}/coverage-filtered.info"
+genhtml -o "${HTML_RESULTS}" "${COV_DIR}/coverage-filtered.info" -s
  
 # Reset our counts
 lcov -d "${COV_DIR}" -z
  
 # Open the index.html
-open "${HTML_RESULTS}/index.html"
+open "${HTML_RESULTS}/index.html" &
